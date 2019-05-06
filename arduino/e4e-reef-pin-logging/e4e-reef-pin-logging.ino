b07a65554d6c0afb1bd1d8cec4e4137f8f2cd253
AnfengAlex <40374253+AnfengAlex@users.noreply.github.com>
2019-05-06 14:35:53 -0700

    Update e4e-reef-pin-logging.ino
    fixed the bugs, still minor issues

--- arduino/e4e-reef-pin-logging/e4e-reef-pin-logging.ino
+++ arduino/e4e-reef-pin-logging/e4e-reef-pin-logging.ino
@@ -6,10 +6,13 @@
 #define DEPTH_DISPLAY_DRIVER_ADDR 0x38
 #define DECIMAL_PT_REG 0x24
 #define SECOND_DECIMAL 0x40
-
-#define SEA_LVL_PRESSURE 1013.25
+#define SEA_LVL_PRESSURE 970
 #define WATER_DENSITY 1029 
 #define G 9.8
+byte digits[] = {0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b};
+double depth;
+int display_depth;
+int decimal_point;
 
 // Begin class with selected address
 // available addresses (selected by jumper on board) 
@@ -17,40 +20,130 @@
 
 //  ADDRESS_HIGH = 0x76
 //  ADDRESS_LOW  = 0x77
-
+int set_number(int number);
 MS5803 sensor(ADDRESS_HIGH);
-
-byte digits[] = {0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b};
+void set_digit(uint8_t displayRegOffset, uint8_t index);
+void i2c_send_byte(uint8_t address, uint8_t instruction, uint8_t value);
 
 //Create variables to store results
 float temperature_c, temperature_f;
 double pressure_abs, pressure_relative, altitude_delta, pressure_baseline;
 
-double depth;
-int display_depth;
-int decimal_point;
-
 // Create Variable to store altitude in (m) for calculations;
 double base_altitude = 1655.0; // Altitude of SparkFun's HQ in Boulder, CO. in (m)
-double sea_level_pressure = 1013.25; // sea level pressure is 1013.25mb
 
-
-void i2c_send_byte(uint8_t address, uint8_t instruction, uint8_t value) {
-  Wire.beginTransmission(address);
-  Wire.write(instruction);
-  Wire.write(value);
-  Wire.endTransmission();
-}
-
-void i2c_send_bytes(uint8_t address, uint8_t instruction, uint8_t *data, int len) {
+void i2c_send(uint8_t address, uint8_t instruction, uint8_t *data, int len) {
   Wire.beginTransmission(address);
   Wire.write(instruction);
   Wire.write(data,len);
   Wire.endTransmission();
 }
 
-void set_digit(uint8_t displayRegOffset, uint8_t index) {
-  i2c_send_byte(DEPTH_DISPLAY_DRIVER_ADDR, 0x20 + displayRegOffset, digits[index]);
+void setup() {
+  Serial.begin(9600);
+  //Retrieve calibration constants for conversion math.
+  sensor.reset();
+  sensor.begin();
+
+  pressure_baseline = sensor.getPressure(ADC_4096);
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin()) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");  
+  Wire.begin();
+
+  i2c_send_byte(DEPTH_DISPLAY_DRIVER_ADDR, 0x24, 0x20); 
+}
+
+void loop() {
+
+  // To measure to higher degrees of precision use the following sensor settings:
+  // ADC_256 
+  // ADC_512 
+  // ADC_1024
+  // ADC_2048
+  // ADC_4096
+
+  // Read temperature from the sensor in deg C. This operation takes about 
+  temperature_c = sensor.getTemperature(CELSIUS, ADC_512);
+
+  // Read temperature from the sensor in deg F. Converting
+  // to Fahrenheit is not internal to the sensor.
+  // Additional math is done to convert a Celsius reading.
+  temperature_f = sensor.getTemperature(FAHRENHEIT, ADC_512);
+
+  // Read pressure from the sensor in mbar.
+  pressure_abs = sensor.getPressure(ADC_4096);
+
+  // Let's do something interesting with our data.
+
+  // Convert abs pressure with the help of altitude into relative pressure
+  // This is used in Weather stations.
+  pressure_relative = sealevel(pressure_abs, base_altitude);
+
+  // Taking our baseline pressure at the beginning we can find an approximate
+  // change in altitude based on the differences in pressure.   
+  altitude_delta = altitude(pressure_abs , pressure_baseline);
+
+  // Report values via UART
+//  Serial.print("Temperature C = ");
+//  Serial.println(temperature_c);
+//  
+//  Serial.print("Temperature F = ");
+//  Serial.println(temperature_f);
+//
+//  Serial.print("Pressure abs (mbar)= ");
+//  Serial.println(pressure_abs);
+//
+//  Serial.print("Pressure relative (mbar)= ");
+//  Serial.println(pressure_relative); 
+//
+//  Serial.print("Altitude change (m) = ");
+//  Serial.println(altitude_delta); 
+
+  depth = (pressure_abs - SEA_LVL_PRESSURE)*100/(WATER_DENSITY * G);
+  display_depth = (int) (depth * 10000);
+  Serial.println(display_depth);
+  set_number(display_depth);
+/*  File dataFile = SD.open("data.txt",FILE_WRITE);
+
+  if (dataFile) {
+    char buf[10];
+    char buf2[10];
+    dtostrf(pressure_relative,4,2,buf);
+    sprintf(buf2,"%s\n",buf);
+    dataFile.write(buf2);
+    Serial.print(buf2);
+    dataFile.close();
+  } else {
+    Serial.println("error opening file");
+  }
+*/
+  delay(200);
+
+}
+
+
+// Thanks to Mike Grusin for letting me borrow the functions below from 
+// the BMP180 example code. 
+
+ double sealevel(double P, double A)
+// Given a pressure P (mbar) taken at a specific altitude (meters),
+// return the equivalent pressure (mbar) at sea level.
+// This produces pressure readings that can be used for weather measurements.
+{
+  return(P/pow(1-(A/44330.0),5.255));
+}
+
+
+double altitude(double P, double P0)
+// Given a pressure measurement P (mbar) and the pressure at a baseline P0 (mbar),
+// return altitude (meters) above baseline.
+{
+  return(44330.0*(1-pow(P/P0,1/5.255)));
 }
 
 int set_number(int number) {
@@ -69,80 +162,15 @@ int set_number(int number) {
   set_digit(2,tens);
   set_digit(1,hundreds);
   set_digit(0,thousands);
+}
 
-  Wire.beginTransmission(DEPTH_DISPLAY_DRIVER_ADDR);
-  Wire.write(DECIMAL_PT_REG);
-  Wire.write(SECOND_DECIMAL);
+void set_digit(uint8_t displayRegOffset, uint8_t index) {
+  i2c_send_byte(DEPTH_DISPLAY_DRIVER_ADDR, 0x20 + displayRegOffset, digits[index]);
+}
+
+void i2c_send_byte(uint8_t address, uint8_t instruction, uint8_t value) {
+  Wire.beginTransmission(address);
+  Wire.write(instruction);
+  Wire.write(value);
   Wire.endTransmission();
-}
-
-void initDepthDisplay() {
-  Wire.beginTransmission(DEPTH_DISPLAY_DRIVER_ADDR);
-  Wire.write(0x01); // register decode mode
-  Wire.write(0x00); // disable decode mode for all digits
-  Wire.write(0x3f); // intensity max
-  Wire.write(0x03); // scan limit 3
-  Wire.write(0x03); // normal operation
-  Wire.endTransmission();
-}
-
-void setup() {
-  Serial.begin(9600);
-  //Retrieve calibration constants for conversion math.
-  sensor.reset();
-  sensor.begin();
-    
-  pressure_baseline = sensor.getPressure(ADC_4096);
-  Serial.print("Initializing SD card...");
-
-  if (!SD.begin(4)) {
-    Serial.println("initialization failed!");
-    while (1);
-  }
-  Serial.println("initialization done.");  
-  Wire.begin();
-  initDepthDisplay();
-}
-
-void loop() {
-  
-  // To measure to higher degrees of precision use the following sensor settings:
-  // ADC_256 
-  // ADC_512 
-  // ADC_1024
-  // ADC_2048
-  // ADC_4096
-    
-  // Read temperature from the sensor in deg C. This operation takes about 
-  temperature_c = sensor.getTemperature(CELSIUS, ADC_512);
-  
-  // Read temperature from the sensor in deg F. Converting
-  // to Fahrenheit is not internal to the sensor.
-  // Additional math is done to convert a Celsius reading.
-  temperature_f = sensor.getTemperature(FAHRENHEIT, ADC_512);
-  
-  // Read pressure from the sensor in mbar.
-  pressure_abs = sensor.getPressure(ADC_4096);
-    
-  depth = (pressure_abs - SEA_LVL_PRESSURE)*100/(WATER_DENSITY * G);
-  display_depth = (int) (depth * 100);
-
-  set_number(display_depth);
-
-  File dataFile = SD.open("data.txt",FILE_WRITE);
-
-  if (dataFile) {
-    char buf[10];
-    char buf2[10];
-    dtostrf(depth,4,2,buf);
-    sprintf(buf2,"%s\n",buf);
-    dataFile.write(buf2);
-    Serial.print(buf2);
-    dataFile.close();
-  } else {
-    Serial.println("error opening file");
-  }
-
-  delay(200);
-
 }

c7532a6434fc51397c99ad7ff0a7a9e834b7ef68
Falafel72 <kylehu2000@gmail.com>
2019-05-22 17:23:42 -0700

    added external interrupts for charging and entering water

--- arduino/e4e-reef-pin-logging/e4e-reef-pin-logging.ino
+++ arduino/e4e-reef-pin-logging/e4e-reef-pin-logging.ino
@@ -1,14 +1,27 @@
+#include <LowPower.h>
 #include <SPI.h>
 #include <SD.h>
 #include <Wire.h>
 #include <SparkFun_MS5803_I2C.h>
+#include <SoftwareSerial.h>
 
 #define DEPTH_DISPLAY_DRIVER_ADDR 0x38
 #define DECIMAL_PT_REG 0x24
 #define SECOND_DECIMAL 0x40
 #define SEA_LVL_PRESSURE 970
 #define WATER_DENSITY 1029 
+
 #define G 9.8
+
+#define CHARGE_STAT_PIN 7
+#define WET_DETECTOR_PIN 6
+#define BLUETOOTH_RX 3
+#define BLUETOOTH_TX 2
+
+#define LOW_POWER_STATE 0
+#define LOGGING_STATE 1
+#define BLUETOOTH_STATE 2
+
 byte digits[] = {0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b};
 double depth;
 int display_depth;
@@ -20,6 +33,9 @@ int decimal_point;
 
 //  ADDRESS_HIGH = 0x76
 //  ADDRESS_LOW  = 0x77
+
+
+
 int set_number(int number);
 MS5803 sensor(ADDRESS_HIGH);
 void set_digit(uint8_t displayRegOffset, uint8_t index);
@@ -32,6 +48,25 @@ double pressure_abs, pressure_relative, altitude_delta, pressure_baseline;
 // Create Variable to store altitude in (m) for calculations;
 double base_altitude = 1655.0; // Altitude of SparkFun's HQ in Boulder, CO. in (m)
 
+SoftwareSerial BT (BLUETOOTH_RX, BLUETOOTH_TX);
+
+int state = 0;
+
+void charging() {
+  // charging interrupt
+  // should change state variable
+}
+
+void not_charging() {
+  // switches when charging stops
+  // should change state variable
+}
+
+void in_water() {
+  // triggers when in water
+  // should change state variable
+}
+
 void i2c_send(uint8_t address, uint8_t instruction, uint8_t *data, int len) {
   Wire.beginTransmission(address);
   Wire.write(instruction);
@@ -39,27 +74,7 @@ void i2c_send(uint8_t address, uint8_t instruction, uint8_t *data, int len) {
   Wire.endTransmission();
 }
 
-void setup() {
-  Serial.begin(9600);
-  //Retrieve calibration constants for conversion math.
-  sensor.reset();
-  sensor.begin();
-
-  pressure_baseline = sensor.getPressure(ADC_4096);
-  Serial.print("Initializing SD card...");
-
-  if (!SD.begin()) {
-    Serial.println("initialization failed!");
-    while (1);
-  }
-  Serial.println("initialization done.");  
-  Wire.begin();
-
-  i2c_send_byte(DEPTH_DISPLAY_DRIVER_ADDR, 0x24, 0x20); 
-}
-
-void loop() {
-
+void log_data() {
   // To measure to higher degrees of precision use the following sensor settings:
   // ADC_256 
   // ADC_512 
@@ -88,27 +103,11 @@ void loop() {
   // change in altitude based on the differences in pressure.   
   altitude_delta = altitude(pressure_abs , pressure_baseline);
 
-  // Report values via UART
-//  Serial.print("Temperature C = ");
-//  Serial.println(temperature_c);
-//  
-//  Serial.print("Temperature F = ");
-//  Serial.println(temperature_f);
-//
-//  Serial.print("Pressure abs (mbar)= ");
-//  Serial.println(pressure_abs);
-//
-//  Serial.print("Pressure relative (mbar)= ");
-//  Serial.println(pressure_relative); 
-//
-//  Serial.print("Altitude change (m) = ");
-//  Serial.println(altitude_delta); 
-
   depth = (pressure_abs - SEA_LVL_PRESSURE)*100/(WATER_DENSITY * G);
   display_depth = (int) (depth * 10000);
   Serial.println(display_depth);
   set_number(display_depth);
-/*  File dataFile = SD.open("data.txt",FILE_WRITE);
+  File dataFile = SD.open("data.txt",FILE_WRITE);
 
   if (dataFile) {
     char buf[10];
@@ -121,9 +120,47 @@ void loop() {
   } else {
     Serial.println("error opening file");
   }
-*/
-  delay(200);
 
+  delay(200);
+}
+
+void send_data() {
+  // Should send current data.txt file from SD card over bluetooth.
+}
+
+void setup() {
+  
+  pinMode(CHARGE_STAT_PIN, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(CHARGE_STAT_PIN), charging, FALLING);
+  attachInterrupt(digitalPinToInterrupt(CHARGE_STAT_PIN), charging, RISING);
+  attachInterrupt(digitalPinToInterrupt(WET_DETECTOR_PIN), in_water, RISING);
+  
+  Serial.begin(9600);
+  //Retrieve calibration constants for conversion math.
+  sensor.reset();
+  sensor.begin();
+
+  pressure_baseline = sensor.getPressure(ADC_4096);
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin()) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");  
+  Wire.begin();
+
+  i2c_send_byte(DEPTH_DISPLAY_DRIVER_ADDR, 0x24, 0x20); 
+}
+
+void loop() {
+
+  while (state) {
+    // either log data or send data here
+  }
+
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
 }
 
 
